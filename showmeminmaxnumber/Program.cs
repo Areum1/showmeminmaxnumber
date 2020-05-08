88b4bc69f92323f960e563f25776d04ccbdf595a
Areum1 <s2018s10@e-mirim.hs.kr>
2020-05-08 11:22:38 +0900

    초기 커밋.

---
+++ showmeminmaxnumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showmeminmaxnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0, max, min;
+
+            Console.Write("숫자를 입력해주세요: ");
+            input = int.Parse(Console.ReadLine());
+            max = min = input;
+            for (int i=0; i<4; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                input = int.Parse(Console.ReadLine());
+
+                if (max < input) 
+                    max = input;
+
+                if (min > input)
+                    min = input;
+            }
+            Console.WriteLine("========================");
+            Console.WriteLine("가장 큰 수: "+max);
+            Console.WriteLine("가장 작은 수: " + min);
+        }
+    }
+}
